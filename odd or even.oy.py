810eed59ee4680d1e3a183018b202e8b583475aa
tinahong94 <tina_hong0302@outlook.com>
2017-04-13 17:30:07 -0500

    odd or even

---
+++ odd or even.oy.py
@@ -0,0 +1,10 @@
+number = int(input("Give me a number: "))
+
+if number % 2 == 0:
+    if number % 4 == 0:
+        print("This number is even and is a multiple of 4")
+    print("This number is even")
+else:
+    print("This number is odd")
+
+# print("Your number is " + number)3
